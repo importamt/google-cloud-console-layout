764d6d07c3847b914f878c85863951e72d33949f
importamt <dlaqhxoa@gmail.com>
2021-09-18 01:20:27 +0900

    mod hamburgerMenu like google cloud console ...
    dim 추가 및 메인 메뉴에 해당하는 내용 표출할 수 있게끔 작업

--- src/components/organisms/hamburger-menu/HamburgerMenu.tsx
+++ src/components/organisms/hamburger-menu/HamburgerMenu.tsx
@@ -1,28 +1,65 @@
 import styled from "styled-components";
+import {Menu} from "../../../store/view/console";
+import {SubMenuItem} from "../../molecules";
 
 interface IHamburgerMenu {
-    isShow: boolean
+    isShow: boolean,
+    menus: Menu[] | null
 }
 
-export const HamburgerMenu = ({isShow}: IHamburgerMenu) => {
+export const HamburgerMenu = ({isShow, menus}: IHamburgerMenu) => {
     return (
-        <StyledHamburgerMenu isShow={isShow}>
-            abc
-        </StyledHamburgerMenu>
+        <StyledHamburgerMenuWrap isShow={isShow}>
+            <StyledHamburgerMenu isShow={isShow}>
+                {menus?.map(menu => <SubMenuItem key={menu.id} menu={menu}/>)}
+            </StyledHamburgerMenu>
+            <StyledHamburgerMenuDim isShow={isShow}/>
+        </StyledHamburgerMenuWrap>
     )
 }
 
-const StyledHamburgerMenu = styled.aside<IHamburgerMenu>`
+const StyledHamburgerMenuDim = styled.aside<{ isShow: boolean }>`
   position: absolute;
-  width: 350px;
-  height: 100%;
-  left: ${({isShow}) => isShow?0:'-350px'};
   top: 0;
-  
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+
+  background: #00000080;
+  opacity: ${({isShow}) => isShow ? 1 : 0};
+  z-index: 99;
+  transition: opacity 500ms;
+`
+
+const StyledHamburgerMenu = styled.ul<{ isShow: boolean }>`
+  pointer-events: auto;
+
+  position: absolute;
+  width: 275px;
+  height: calc(100% - 100px);
+  padding: 50px 0;
+
+  left: ${({isShow}) => isShow ? 0 : '-350px'};
+  top: 0;
+
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
   background: palegreen;
-  
+
   transition: left 500ms;
+
+  z-index: 100;
+`
+
+const StyledHamburgerMenuWrap = styled.section<{ isShow: boolean }>`
+  pointer-events: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+
+    //z-index: ${({isShow}) => isShow ? 0 : -1};
 `
