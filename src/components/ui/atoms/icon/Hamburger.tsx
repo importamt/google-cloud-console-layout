32c1ce13abf4190e4eee1421f7d39bbb923483db
importamt <dlaqhxoa@gmail.com>
2021-09-17 16:19:42 +0900

    rm icons ...
    아이콘 적용방법은 다시 고민해볼 것

--- src/components/ui/atoms/icon/Hamburger.tsx
+++
@@ -1,4 +0,0 @@
-import {Menu} from '@styled-icons/entypo/Menu'
-import {IIcon} from "./index";
-
-export const Hamburger = (props: IIcon) => <Menu {...props}/>
--- src/components/ui/atoms/icon/index.ts
+++
@@ -1,8 +0,0 @@
-import {MouseEventHandler} from "react";
-import {StyledIconProps} from "@styled-icons/styled-icon";
-
-export {Icon} from './Icon'
-export {Hamburger} from './Hamburger'
-
-export interface IIcon extends StyledIconProps{
-}
