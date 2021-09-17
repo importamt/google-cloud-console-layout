d912f8287142f24161f5aceb4adf2896ab40719e
importamt <dlaqhxoa@gmail.com>
2021-09-17 16:21:14 +0900

    mod store settings...

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,15 @@
 import {combineReducers, configureStore, EnhancedStore} from "@reduxjs/toolkit";
-import {all, fork} from "@redux-saga/core/effects";
+import {all} from "@redux-saga/core/effects";
 import createSagaMiddleware from "redux-saga";
+import {UserReducer} from "./data/user";
+import {ConsoleReducer} from "./view/console";
 
-const dataReducer = combineReducers({})
-const viewReducer = combineReducers({})
+const dataReducer = combineReducers({
+    user: UserReducer
+})
+const viewReducer = combineReducers({
+    console: ConsoleReducer
+})
 const rootReducer = combineReducers({
     view: viewReducer,
     data: dataReducer,
