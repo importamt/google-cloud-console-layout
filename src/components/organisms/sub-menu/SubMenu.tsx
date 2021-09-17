89b7020c85c2f2137aa3f2d024f6b5e759f79095
importamt <dlaqhxoa@gmail.com>
2021-09-18 01:25:23 +0900

    mod sub menu

--- src/components/organisms/sub-menu/SubMenu.tsx
+++ src/components/organisms/sub-menu/SubMenu.tsx
@@ -1,17 +1,23 @@
 import styled from "styled-components";
+import {Menu} from "../../../store/view/console";
+import {SubMenuItem} from "../../molecules";
 
+interface ISubMenu {
+    menus: Menu[] | null
+}
 
-export const SubMenu = () => {
+export const SubMenu = ({menus}: ISubMenu) => {
     return (
         <StyledSubMenu>
-            123
+            {menus?.map(menu => <SubMenuItem key={menu.id} menu={menu}/>)}
         </StyledSubMenu>
     )
 }
 
-const StyledSubMenu = styled.aside`
+const StyledSubMenu = styled.ul`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  //justify-content: center;
+  //align-items: center;
+  flex-direction: column;
   background: pink;
 `
