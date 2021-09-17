d432d061d96b0be940bd3e0361e577e011107f8f
importamt <dlaqhxoa@gmail.com>
2021-09-17 16:21:32 +0900

    add store(provider) and router

--- index.tsx
+++ index.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {AboutMe} from "./src/pages";
+import {AboutMe, Home} from "./src/pages";
+import {Provider} from "react-redux";
+import {BrowserRouter as Router, Route, Switch,} from "react-router-dom";
+import { createStore } from './src/store';
+import {GlobalStyles} from "./src/styles";
 
-// const App = () => {
-
-    // return (
-    //
-    // )
-// }
-
-
+const store = createStore()
 ReactDOM.render(
-    <AboutMe/>,
+    <Provider store={store}>
+        {/*Common Components*/}
+        <GlobalStyles/>
+
+        {/*Views*/}
+        <Router>
+            <Switch>
+                <Route>
+                    <Home/>
+                </Route>
+                <Route path={"/home"}>
+                    <Home/>
+                </Route>
+                <Route path={"/map"}>
+                </Route>
+                <Route path={"/user"}>
+                </Route>
+            </Switch>
+        </Router>
+    </Provider>,
     document.getElementById('root')
-)
+);
