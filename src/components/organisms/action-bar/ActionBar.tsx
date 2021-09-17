c2731847451fd0e19e7b5f012be080f458c6f4f2
importamt <dlaqhxoa@gmail.com>
2021-09-18 01:19:46 +0900

    add ActionBar
    이름 표출 및 가지고 있는 action들 그리기

--- src/components/organisms/action-bar/ActionBar.tsx
+++ src/components/organisms/action-bar/ActionBar.tsx
@@ -1,17 +1,29 @@
 import styled from "styled-components";
+import {Menu} from "../../../store/view/console";
+import {ActionBarItem} from "../../molecules";
 
+interface IActionBar {
+    menu: Menu | null | undefined
+}
 
-export const ActionBar = () => {
+export const ActionBar = ({menu}: IActionBar) => {
     return (
         <StyledActionBar>
-            456
+            <h1>{menu?.name}</h1>
+            {menu?.actions?.map(action => <ActionBarItem action={action}/>)}
         </StyledActionBar>
     )
 }
 
 const StyledActionBar = styled.aside`
   display: flex;
-  justify-content: center;
+  //justify-content: center;
   align-items: center;
   background: red;
+
+  & h1 {
+    font-size: 20px;
+    color: black;
+    margin: 0 30px;
+  }
 `
