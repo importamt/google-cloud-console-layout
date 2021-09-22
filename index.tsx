b543f8806e91054dbbfc6f95392bd31ba9820f48
botem <dlaqhxoa@gmail.com>
2021-09-23 00:31:25 +0900

    add global style for font and add default route component

--- index.tsx
+++ index.tsx
@@ -5,6 +5,7 @@ import {Provider} from "react-redux";
 import {BrowserRouter as Router, Route, Switch,} from "react-router-dom";
 import {createStore} from './src/store';
 import {GlobalStyles} from "./src/styles";
+import {GlobalFonts} from "./src/styles/GlobalFonts";
 
 const store = createStore()
 ReactDOM.render(
@@ -14,6 +15,8 @@ ReactDOM.render(
             <Provider store={store}>
                 {/*Common Components*/}
                 <GlobalStyles/>
+                <GlobalFonts/>
+                <Route path={"/"} exact component={Home}/>
                 <Route path={"/home"} component={Home}/>
                 <Route path={"/map"} component={Map}/>
                 <Route path={"/user"} component={User}/>
