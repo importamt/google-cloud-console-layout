3243c7977c06d68290e0759c10d27ea14c42e30e
botem <dlaqhxoa@gmail.com>
2021-09-23 00:29:23 +0900

    add intro content (png)
    들어가며 보여질 내용은 깃허브의 readMe 캡쳐본

--- src/pages/home/Intro.tsx
+++ src/pages/home/Intro.tsx
@@ -3,6 +3,8 @@ import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../store";
 import {useMount} from "../../hooks/useMount";
 import {MENU_INFO, setSelectedMenuId} from "../../store/view/console";
+import styled from "styled-components";
+import introImage from './intro.png'
 
 export const Intro = () => {
     const dispatch = useDispatch()
@@ -15,6 +17,15 @@ export const Intro = () => {
     const menus = useSelector((state: RootState) => state.view.console.menus)
 
     return <Console selectedMenuId={selectedMenuId} menus={menus}>
-        <></>
+        <StyledIntro/>
     </Console>
 }
+
+const StyledIntro = styled.figure`
+  width: calc(100% - 100px);
+  height: calc(100% - 100px);
+  margin: 50px;
+  
+  background: url(${introImage}) no-repeat;
+  background-size: 100% 100%;
+`
